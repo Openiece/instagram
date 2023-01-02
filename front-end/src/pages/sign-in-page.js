7c79a3add6e87480d6c383c8b58f1b4adec7fdef
Enkhmandakh <Enkhmandakh@Buynkhishigs-iMac.local>
2023-01-02 18:04:42 +0800

    sign in frontend

--- front-end/src/pages/sign-in-page.js
+++ front-end/src/pages/sign-in-page.js
@@ -1,9 +1,41 @@
-import React from 'react';
-
+import React from "react";
+import img from "../photos/instagraam.png";
+import "../styles/sign-in.css";
+import img2 from "../photos/logo-ig.png";
 export const SignInPage = () => {
-    return (
-        <div>
-            Sign in
+  return (
+    <div className="allinone">
+
+
+
+      <img className="ip" src={img} alt=""></img>
+
+
+
+      <div className="con">
+        <img className="ig" src={img2} alt=""></img>
+        <input className="email" type="email" placeholder=" Email"></input>
+        <input className="pass" type="password" placeholder=" Password"></input>
+        <div className="button">Log in</div>
+        <div className="or">OR</div>
+        <div className="fb">
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="fb-login"> Log in with Facebook</a>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="trauma">Forgot password?</a>
         </div>
-    )
-}
+      </div>
+
+      <div className="con2">
+        <div className="all">
+          <div className="acc">Don't have an account?</div>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="sign-up">Sign up</a>
+        </div>
+      </div>
+
+      <div className="half">
+        <div className="app">Get the app.</div>
+        <img alt="app" className="appstore"src="https://static.cdninstagram.com/rsrc.php/v3/yt/r/Yfc020c87j0.png"></img>
+        <img alt="app"className="googleplay"src="https://static.cdninstagram.com/rsrc.php/v3/yz/r/c5Rp7Ym-Klz.png"></img>
+      </div>
+    </div>
+  );
+};
